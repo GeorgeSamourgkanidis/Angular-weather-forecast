e9993388797cf87de89205302a3f8e19403f5a12
George Samourgkanidis <giorgossamou1@gmail.com>
2023-01-08 22:04:49 +0200

    add api key

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   openWeatherApiUrl: "https://api.openweathermap.org/data/2.5/weather",
-  openWeatherApiKey: "*"
+  openWeatherApiKey: "d0e8f8b3c5301b22bd2368ee271f3c09"
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
   openWeatherApiUrl: 'https://api.openweathermap.org/data/2.5/weather',
-  openWeatherApiKey: "*"
+  openWeatherApiKey: "d0e8f8b3c5301b22bd2368ee271f3c09"
 
 };
